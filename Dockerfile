056d8859e713c62af30e0ed79f316c77bfebbe15
sinzy <sinzy0925@gmail.com>
2024-11-20 21:52:24 +0900

    message

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Pythonのベースイメージを作成
+FROM python:3.11.3
+# 作業ディレクトリを指定
+WORKDIR /app
+# 依存関係ファイルをコピー
+COPY requirements.txt .
+# 依存関係をインストール
+RUN pip install --no-cache-dir -r requirements.txt
+# アプリケーションファイルのコピー
+COPY . .
+# Uvicornを使用してアプリケーションを実行（JSON形式に修正）
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080"]
---
+++ app/main.py
@@ -0,0 +1,51 @@
+import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from youtube_transcript_api import YouTubeTranscriptApi
+import json
+
+load_dotenv()
+
+app = FastAPI()
+
+# CORSミドルウェアを追加して、他のドメインからのリクエストを受け入れられるように設定
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 許可するオリジンを指定
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# 字幕を取得する関数
+def get_transcript(video_id: str):
+    try:
+        # 字幕を取得 (日本語字幕を優先)
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['ja'])
+
+        text = ""
+        if transcript:
+            for item in transcript:
+                text += item["text"]
+        else:
+            print("字幕が見つかりませんでした。")
+
+        text1 = {"text": text}
+        # 字幕のリストを返す
+        return json.dumps(text1, ensure_ascii=False)
+    except Exception as e:
+        print(f"字幕の取得に失敗しました: {e}")
+        return {"error": "字幕の取得に失敗しました"}
+
+# videoIdをクエリパラメータとして受け取るエンドポイント
+@app.get("/transcript")
+async def get_video_transcript(videoId: str):
+    res = get_transcript(videoId)
+    return res
